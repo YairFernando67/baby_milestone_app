28ecc997397fc1505bd5a16ae1a568fabd185077
Yair Fernando Facio Fraga <yair.facio11@gmail.com>
2020-02-03 23:57:51 -0600

    Finished the MilestoneList component

--- src/Components/MilestoneList.js
+++ src/Components/MilestoneList.js
@@ -2,12 +2,42 @@ import React, { Component } from 'react'
 import ListItem from './ListItem';
 
 export class MilestoneList extends Component {
+  state = {
+    page: 1,
+    per_page: 4,
+    initial_page: 0,
+    final_page: 4,
+    total_pages: 0
+  }
+
+  componentDidMount() {
+    if (this.props.milestones) {
+      this.setState({ total_pages: Math.ceil(this.props.milestones.length / this.state.per_page )})
+      if (this.props.milestones[0].skill_id === 23) {
+        localStorage.stand_up = JSON.stringify(this.props.milestones);
+      }
+      if (this.props.milestones[0].skill_id === 2) {
+        localStorage.secure_attachment = JSON.stringify(this.props.milestones);
+      }
+    }
+  }
+
+  nextPage = () => {
+    const { page, initial_page, per_page, final_page } = this.state;
+    this.setState({ page: page + 1, initial_page: initial_page + per_page, final_page: final_page + per_page })
+  }
+  
+  prevPage = () => {
+    const { page, initial_page, per_page, final_page } = this.state;
+    this.setState({ page: page -1, initial_page: initial_page - per_page, final_page: final_page - per_page })
+  }
 
   renderContent = () => {
+    const { initial_page, final_page } = this.state
     if (this.props.milestones) {
       return(
         <div>
-          {this.props.milestones.map(mil => (
+          {this.props.milestones.slice(initial_page, final_page).map(mil => (
             <ListItem key={mil.id} milestone={mil} age_range={this.props.age_range} />
           ))}
         </div>
@@ -15,9 +45,19 @@ export class MilestoneList extends Component {
     }
   }
   render() {
+    const { page, initial_page, per_page, total_pages } = this.state;
     return (
       <div>
         {this.renderContent()}
+        <Pagination 
+          total_pages={total_pages}
+          prevPage={this.prevPage}
+          nextPage={this.nextPage}
+          initial_page={initial_page}
+          page={page}
+          per_page={per_page}
+          milestone={this.props.milestones}
+        />
       </div>
     )
   }
