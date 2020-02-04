6cd0da8341af3c237d71ca4a0b26ad6f3c5ea3c6
Yair Fernando Facio Fraga <yair.facio11@gmail.com>
2020-02-03 23:21:05 -0600

    Finished MilestoneOne component

--- src/Components/MilestoneOne.js
+++ src/Components/MilestoneOne.js
@@ -1,13 +1,41 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux';
+import styled from 'styled-components'
+
+const Spinner = styled.div`
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0 auto;
+  top: 75%;
+  right: 50%;
+`
 
 export class MilestoneOne extends Component {
   render() {
+    const { milestones, main_info } = this.props.secure_attachment;
+    let secure_attachment = localStorage.getItem('secure_attachment')
+    secure_attachment = JSON.parse(secure_attachment)
+    if (this.props.secure_attachment.milestones) {
+      if (secure_attachment === null) {
+        return <MilestoneList milestones={milestones} age_range={main_info.age_range} />
+      } else {
+        return <MilestoneList milestones={secure_attachment} age_range={main_info.age_range} />
+      }
+    }
     return (
-      <div>
-        MilestoneOne
-      </div>
+      <Spinner className="spinner-grow text-danger" role="status">
+        <span className="sr-only">Loading...</span>
+      </Spinner>
     )
   }
 }
 
-export default MilestoneOne
+const mapStateToProps = state => {
+  return {
+    secure_attachment: state.secure_attachment
+  }
+}
+
+export default connect(mapStateToProps)(MilestoneOne)
