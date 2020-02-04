598279614e917473de6a139bcead0001039922c3
Yair Fernando Facio Fraga <yair.facio11@gmail.com>
2020-02-03 23:47:01 -0600

    Finished reducers for edit

--- src/Reducers/secureAttachmentReducer.js
+++ src/Reducers/secureAttachmentReducer.js
@@ -1,11 +1,15 @@
 import {
-  FETCH_SECURE_ATTACHMENT_MILESTONE
+  FETCH_SECURE_ATTACHMENT_MILESTONE,
+  EDIT_SECURE_ATTACHMENT_MILESTONE
 } from '../Types';
 
 export default (state = {}, action) => {
   switch (action.type) {
     case FETCH_SECURE_ATTACHMENT_MILESTONE:
       return action.payload;
+    case EDIT_SECURE_ATTACHMENT_MILESTONE:
+      localStorage.secure_attachment = JSON.stringify(action.payload);
+      return {...state, milestones: action.payload}
     default:
       return state;
   }
--- src/Reducers/standUpReducer.js
+++ src/Reducers/standUpReducer.js
@@ -1,11 +1,15 @@
 import {
-  FETCH_STAND_UP_MILESTONE
+  FETCH_STAND_UP_MILESTONE,
+  EDIT_STAND_UP_MILESTONE
 } from '../Types'
 
 export default (state = {}, action) => {
   switch (action.type) {
     case FETCH_STAND_UP_MILESTONE:
       return action.payload;
+    case EDIT_STAND_UP_MILESTONE:
+      localStorage.stand_up = JSON.stringify(action.payload);
+      return {...state, milestones: action.payload}
     default:
       return state;
   }
