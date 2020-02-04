f79c792089758854d4ba1d9d018e4328e9a2639a
Yair Fernando Facio Fraga <yair.facio11@gmail.com>
2020-02-03 22:47:34 -0600

    Setting up the first two action creators to fetch the data from the api

--- src/Actions/index.js
+++ src/Actions/index.js
@@ -1,8 +1,45 @@
+import kinedu from '../API/kinedu';
 import {
   FETCH_SECURE_ATTACHMENT_MILESTONE,
   FETCH_STAND_UP_MILESTONE
 } from '../Types'
 
 export default fetchSecureAttachmentSkill = () => async (dispatch) => {
-  
+  const response = kinedu.get('/2/milestones');
+  const skillTwo = createSkill(response);
+  dispatch({
+    type: FETCH_SECURE_ATTACHMENT_MILESTONE,
+    payload: skillTwo
+  })
+}
+
+export default fetchStandUpSkill = () => async (dispatch) => {
+  const response = kinedu.get('/23/milestones');
+  const skillOne = createSkill(response);
+  dispatch({
+    type: FETCH_STAND_UP_MILESTONE,
+    payload: skillOne
+  })
+}
+
+const createSkill = (response) => {
+  const rst = response.data.data.skill;
+  rst.milestones.map(mil => {
+    mil.answer = "Not Answer"
+  })
+  const { id, area_id, age_range, parent_skill_id, title, description } = rst;
+  const skill = {
+    main_info: {
+      id,
+      area_id,
+      age_range,
+      parent_skill_id,
+      title,
+      description
+    },
+    milestones: rst.milestones,
+    finished_assesstment: false
+  }
+
+  return skill
 }
