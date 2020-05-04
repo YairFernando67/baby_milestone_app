ef184aac34c148636bd501cfb11269e6a1bc0659
Yair Fernando Facio Fraga <yair.facio11@gmail.com>
2020-05-04 00:01:45 -0500

    Modified URL

--- src/API/kinedu.js
+++ src/API/kinedu.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 const token = 'token=09d23abf0c1d10e37592819dd8157ee06f22c0d308a8906d21e25c0de4f838859e0d5c1337aca40103b028ec81e948c6be382fce7c82d6ad273ad4fcd16e8f58';
 const header = `Token ${token}`;
 export default axios.create({
-  baseURL: 'http://demo.kinedu.com/api/v3/skills',
+  baseURL: 'https://demo.kinedu.com/api/v3/skills',
   headers:  {Authorization: header}
 })
