9570c3cc5d7820147014aea8f1e83068ac6985e5
Yair Fernando Facio Fraga <yair.facio11@gmail.com>
2020-05-20 17:46:00 -0500

    Fixed issues

--- server.js
+++ server.js
@@ -4,15 +4,9 @@ const path = require('path');
 const app = express();
 
 if (process.env.NODE_ENV !== 'production') {
-<<<<<<< HEAD
   const webpackMiddleware = require('webpack-dev-middleware'); 
   const webpack = require('webpack'); 
   const webpackConfig = require('./webpack.config.js'); 
-=======
-  const webpackMiddleware = require('webpack-dev-middleware');
-  const webpack = require('webpack');
-  const webpackConfig = require('./webpack.config.js');
->>>>>>> ef184aac34c148636bd501cfb11269e6a1bc0659
   app.use(webpackMiddleware(webpack(webpackConfig)));
 } else {
   app.use(express.static('dist'));
@@ -21,8 +15,4 @@ if (process.env.NODE_ENV !== 'production') {
   });
 }
 
-<<<<<<< HEAD
 app.listen(process.env.PORT || 3050, () => console.log('Listening'));
-=======
-app.listen(process.env.PORT || 3050, () => console.log("Listening"));
->>>>>>> ef184aac34c148636bd501cfb11269e6a1bc0659
